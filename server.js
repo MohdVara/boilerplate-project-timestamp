9825259f00c5bdd3d594729a06db187e6663b786
Mohd. Paramasvara <mwara95@gmail.com>
2019-04-19 20:08:44 +0800

    Challenge Microservice timestamp completed

--- server.js
+++ server.js
@@ -1,27 +1,32 @@
+/*jshint esversion: 6 */
 // server.js
-// where your node app starts
+process.env.PORT = 3000;
 
-// init project
 var express = require('express');
 var app = express();
 
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
-
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
-
-// http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+app.get("/api/timestamp", function (req, res) {
+  resultDate = new Date();
+  res.json({
+    unix: resultDate.getTime(), 
+    utc: resultDate.toUTCString()
+  });
 });
 
+app.get("/api/timestamp/:date_string", function (req, res) {
+  let regexUnix = /^\d{10}$/;
+  let date_string = req.params.date_string;
+  let resultDate;
+  if(regexUnix.test(date_string)){
+    resultDate = new Date(parseInt(date_string)*1000); 
+  }else{
+    resultDate = new Date(date_string);
+  }
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({
+    unix: resultDate.getTime(), 
+    utc: resultDate.toUTCString()
+  });
 });
 
 
